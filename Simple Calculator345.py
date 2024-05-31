caafea0ca4a09cf8d2b839ebc4f930450a46867d
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-05-31 19:27:57 +0700

    Number Input

--- Simple Calculator345.py
+++ Simple Calculator345.py
@@ -1,2 +1,6 @@
 
-#! The first commit for this branch
+#TODO : Make a calculator works
+
+#todo: create a basic math operations
+NumberInput1 = float(input("Enter the first number: "))
+NumberInput2 = float(input("Enter the second number: "))
