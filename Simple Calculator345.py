52cb0ebc9d3cea9e3bb32587ed0ac79612f7a58a
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-01 21:30:30 +0700

    A few changes

--- Simple Calculator345.py
+++ Simple Calculator345.py
@@ -2,5 +2,7 @@
 #TODO : Make a calculator works
 
 #todo: create a basic math operations
+print("\nSimple Calculator\n")
+#? Use for loop or while loop
 NumberInput1 = float(input("Enter the first number: "))
 NumberInput2 = float(input("Enter the second number: "))
