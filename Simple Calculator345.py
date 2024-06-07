358db397339bfe257f1f2cd4fb9b82534910391a
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-07 19:52:32 +0700

    Testing AGAIN

--- Simple Calculator345.py
+++ Simple Calculator345.py
@@ -5,5 +5,6 @@
 print("\nSimple Calculator\n")
 #! Use While Loop #Testing
 #? Testing blablabla
+#* TEST
 NumberInput1 = float(input("Enter the first number: "))
 NumberInput2 = float(input("Enter the second number: "))
