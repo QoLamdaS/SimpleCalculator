21c213a12a13ad8b5327444e0af30f96d44d36e3
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-07 19:58:31 +0700

    Delete some testing comments

--- Simple Calculator345.py
+++ Simple Calculator345.py
@@ -3,8 +3,6 @@
 
 #todo: create a basic math operations
 print("\nSimple Calculator\n")
-#! Use While Loop #Testing
-#? Testing blablabla
-#* TEST
+#! Use While Loop
 NumberInput1 = float(input("Enter the first number: "))
 NumberInput2 = float(input("Enter the second number: "))
