40ded0091b9e9efc67612a98cb218c03668d1d12
WillyS <kaffeeunused@gmail.com>
2024-06-16 23:33:27 +0700

    Make a while loop for taking user input (UNFINISHED)

--- Simple Calculator345.py
+++ Simple Calculator345.py
@@ -1,7 +1,5 @@
 
-#TODO : Make a calculator works
-
-#todo: create a basic math operations
+#TODO: create a basic math operations
 print("\nSimple Calculator\n")
 #! Use While Loop
 NumberInput1 = float(input("Enter the first number: "))
