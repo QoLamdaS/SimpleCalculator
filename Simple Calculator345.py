784fd401b186f33b9b3e1a6feaeaa18323700dd5
WillyS <kaffeeunused@gmail.com>
2024-06-16 23:42:57 +0700

    Testing

--- Simple Calculator345.py
+++ Simple Calculator345.py
@@ -7,3 +7,4 @@ while True:
     MathOperator = input("addition (+), subtraction (-), multiplication (*), division (/), modulo (%), floor division (//), and exponentiation (**)")
     #todo: make a if-statements for MathOperator and exit the program
     NumberInput2 = float(input("Enter the second number: "))
+    #! Testing
