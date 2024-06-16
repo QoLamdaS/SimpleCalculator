b488612b8d6158416f73c799c71d2ac36771088d
WillyS <kaffeeunused@gmail.com>
2024-06-16 23:39:45 +0700

    Make a while loop (UNFINISHED)

--- Simple Calculator345.py
+++ Simple Calculator345.py
@@ -1,8 +1,9 @@
 
-#TODO : Make a calculator works
-
-#todo: create a basic math operations
+#TODO: create a basic math operations
 print("\nSimple Calculator\n")
 #! Use While Loop
-NumberInput1 = float(input("Enter the first number: "))
-NumberInput2 = float(input("Enter the second number: "))
+while True:
+    NumberInput1 = float(input("Enter the first number: "))
+    MathOperator = input("addition (+), subtraction (-), multiplication (*), division (/), modulo (%), floor division (//), and exponentiation (**)")
+    #todo: make a if-statements for MathOperator and exit the program
+    NumberInput2 = float(input("Enter the second number: "))
